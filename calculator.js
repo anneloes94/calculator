babccdbefd639c270dd07abfd09221ee4a54c069
Anneloes <anneloes9405@hotmail.com>
2020-05-20 15:56:30 -0400

    simple addition calculator implemented
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- calculator.js
+++ calculator.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
 
+const bodyParser = require("body-parser")
+
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res){
   res.sendFile(`${__dirname}/index.html`)
 })
 
 app.post("/", function(req, res){
-  res.send(`Great, you calculated something`)
+  const num1 = Number(req.body.num1)
+  const num2 = Number(req.body.num2)
+  res.send(`Your answer is ${num1 + num2}`)
 })
 
 app.listen(port, function(){
