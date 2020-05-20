eb527eb0f6b7c37aa89c6e2df107a417299cea52
Anneloes <anneloes9405@hotmail.com>
2020-05-19 20:49:08 -0400

    added response for root, added nodemon
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 const port = process.env.PORT || 3000
 
+app.get("/", function(req, res){
+  res.send("<h1>Hello World</h1>")
+})
+
 app.listen(port, function(){
   console.log(`Server listening on port ${port}`)
 });
